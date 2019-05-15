26a01a269cbc752b2dbffbad8be1df6bc2e6431e
andreyuhai <burak.kaymakci@bil.omu.edu.tr>
2019-05-15 16:46:27 +0200

    Created a loop that always calculates page fault of all algorithms for a given random page reference string

--- src/com/andreyuhai/Main.java
+++ src/com/andreyuhai/Main.java
@@ -1,54 +1,125 @@
 package com.andreyuhai;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
-	// write your code here
+    public static void main(String[] args) {
+
         List<Integer> pageReferenceString = new ArrayList<>();
+        int numOfFrames;
+        int numOfPages;
 
-        pageReferenceString.add(7);
-        pageReferenceString.add(0);
-        pageReferenceString.add(1);
-        pageReferenceString.add(2);
-        pageReferenceString.add(0);
-        pageReferenceString.add(3);
-        pageReferenceString.add(0);
-        pageReferenceString.add(4);
-        pageReferenceString.add(2);
-        pageReferenceString.add(3);
-        pageReferenceString.add(0);
-        pageReferenceString.add(3);
-        pageReferenceString.add(2);
-        pageReferenceString.add(1);
-        pageReferenceString.add(2);
-        pageReferenceString.add(0);
-        pageReferenceString.add(1);
-        pageReferenceString.add(7);
+        while(true) {
+
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter the number of frames: ");
+            numOfFrames= sc.nextInt();
+            System.out.print("Enter the number of pages: ");
+            numOfPages = sc.nextInt();
+
+            for(int i = 0; i < numOfPages; i++) {
+                pageReferenceString.add((int) (Math.random() * numOfPages));
+            }
+
+            System.out.println("Size: " + pageReferenceString.size());
+
+            for (Integer integer : pageReferenceString) {
+                System.out.println(integer);
+            }
 
 
-        FirstInFirstOut fifo = new FirstInFirstOut(3);
-        fifo.setPageReferenceString(pageReferenceString);
 
-        LeastRecentlyUsed lru = new LeastRecentlyUsed(3);
-        lru.setPageReferenceString(pageReferenceString);
+            PageReplacementAlgorithm fifo = new FirstInFirstOut(numOfFrames);
+            fifo.setPageReferenceString(pageReferenceString);
 
-        Optimal optimal = new Optimal(3);
-        optimal.setPageReferenceString(pageReferenceString);
+            PageReplacementAlgorithm lru = new LeastRecentlyUsed(numOfFrames);
+            lru.setPageReferenceString(pageReferenceString);
 
-        OperatingSystem os = new OperatingSystem();
-        os.setPageReplacementAlgorithm(fifo);
-        System.out.println(os.solve());
+            PageReplacementAlgorithm optimal = new Optimal(numOfFrames);
+            optimal.setPageReferenceString(pageReferenceString);
 
-        os.setPageReplacementAlgorithm(lru);
-        System.out.println(os.solve());
+            PageReplacementAlgorithm alru = new ApproximatedLRU(numOfFrames);
+            alru.setPageReferenceString(pageReferenceString);
 
-        os.setPageReplacementAlgorithm(optimal);
-        System.out.println(os.solve());
+            PageReplacementAlgorithm random = new Random(numOfFrames);
+            random.setPageReferenceString(pageReferenceString);
+
+
+            //
+
+            OperatingSystem os = new OperatingSystem();
+            os.setPageReplacementAlgorithm(fifo);
+            System.out.println("[FIFO]\tPage fault: " + os.solve());
+
+            os.setPageReplacementAlgorithm(lru);
+            System.out.println("[LRU]\tPage fault: " + os.solve());
+
+            os.setPageReplacementAlgorithm(optimal);
+            System.out.println("[OPT]\tPage fault: " + os.solve());
+
+            os.setPageReplacementAlgorithm(alru);
+            System.out.println("[ALRU]\tPage fault: " + os.solve());
+
+            os.setPageReplacementAlgorithm(random);
+            System.out.println("[RAND]\tPage fault: " + os.solve());
+
+            int answer;
+            System.out.println("0. Exit");
+            System.out.println("1. Try again");
+            System.out.print("What do you want to do? ");
+            answer = sc.nextInt();
+            if(answer == 0)
+                break;
+        }
+
+
+
+
     }
 }
+
+//    List<Integer> pageReferenceString = new ArrayList<>();
+//    List<Integer> pageReferenceString2 = new ArrayList<>();
+//
+//        pageReferenceString.add(7);
+//                pageReferenceString.add(0);
+//                pageReferenceString.add(1);
+//                pageReferenceString.add(2);
+//                pageReferenceString.add(0);
+//                pageReferenceString.add(3);
+//                pageReferenceString.add(0);
+//                pageReferenceString.add(4);
+//                pageReferenceString.add(2);
+//                pageReferenceString.add(3);
+//                pageReferenceString.add(0);
+//                pageReferenceString.add(3);
+//                pageReferenceString.add(2);
+//                pageReferenceString.add(1);
+//                pageReferenceString.add(2);
+//                pageReferenceString.add(0);
+//                pageReferenceString.add(1);
+//                pageReferenceString.add(7);
+//
+//                //
+//
+//                pageReferenceString2.add(0);
+//                pageReferenceString2.add(4);
+//                pageReferenceString2.add(1);
+//                pageReferenceString2.add(4);
+//                pageReferenceString2.add(2);
+//                pageReferenceString2.add(4);
+//                pageReferenceString2.add(3);
+//                pageReferenceString2.add(4);
+//                pageReferenceString2.add(2);
+//                pageReferenceString2.add(4);
+//                pageReferenceString2.add(0);
+//                pageReferenceString2.add(4);
+//                pageReferenceString2.add(1);
+//                pageReferenceString2.add(4);
+//                pageReferenceString2.add(2);
+//                pageReferenceString2.add(4);
+//                pageReferenceString2.add(3);
+//                pageReferenceString2.add(4);
